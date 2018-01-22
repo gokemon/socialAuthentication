9041b307ca23cce310cf6b6ac436da11d32c5d1e
gokemon <michael.of.gokey@gmail.com>
2018-01-21 23:52:23 -0500

    added Facebook strategy and routes

--- app/routes.js
+++ app/routes.js
@@ -9,35 +9,28 @@ module.exports = function(app, passport) {
     });
 
 
-
     /************************************/
     /*  LOGIN                       *****/
     /************************************/
     // show the login form
     app.get('/login', function(req, res) {
-
         // render the page and pass in any flash data if it exists
         res.render('login.ejs', { message: req.flash('loginMessage') });
     });
 
-    // process the login form
-    // app.post('/login', do all our passport stuff here);
-
-
 
     /************************************/
-    /*  SIGNUP *****/
+    /*  SIGNUP                      *****/
     /************************************/
     // show the signup form
     app.get('/signup', function(req, res) {
-
         // render the page and pass in any flash data if it exists
         res.render('signup.ejs', { message: req.flash('signupMessage') });
     });
 
 
     /************************************/
-    /* process signup form *****/
+    /* process signup form          *****/
     /************************************/
     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect: '/profile', // redirect to the secure profile section
@@ -48,7 +41,7 @@ module.exports = function(app, passport) {
 
 
     /************************************/
-    /*  PROFILE SECTION *****/
+    /*  PROFILE SECTION             *****/
     /************************************/
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
@@ -60,14 +53,30 @@ module.exports = function(app, passport) {
 
 
     /************************************/
+    /*  FACEBOOK ROUTES             *****/
+    /************************************/
+    // route for facebook authentication and login
+    app.get('/auth/facebook', passport.authenticate('facebook', {
+        scope: ['public_profile', 'email']
+    }));
+
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect: '/profile',
+            failureRedirect: '/'
+        })
+    );
+
+
+    /************************************/
     /*  LOGOUT *****/
     /************************************/
     app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
     });
-};
-
+}; // end of module.exports
 
 
 
@@ -80,4 +89,4 @@ function isLoggedIn(req, res, next) {
 
     // if they aren't redirect them to the home page
     res.redirect('/');
-}
+} // end of isLoggedIn function
--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
 
-    'url': 'your-settings-here'
+    'url': 'mongodb://socialAuth:Ch!ck-f!l-@St33rs@ds013004.mlab.com:13004/heroku_9949h93p'
         // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
